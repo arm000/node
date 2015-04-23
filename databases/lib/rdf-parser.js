f5197fe08af5ad6227a11a8f7cb622c9fd40b2c4
Allen Martin <amartin@nvidia.com>
2015-04-23 14:30:28 -0700

    Add views and rdf-parser library for databases examples

---
+++ databases/lib/rdf-parser.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const
+    fs = require('fs'),
+    cheerio = require('cheerio');
+
+module.exports = function(filename, callback) {
+    fs.readFile(filename, function(err, data) {
+	    if (err) { return callback(err); }
+	    let
+	    $ = cheerio.load(data.toString(), {xmlMode: true}),
+	    collect = function(index, elem) {
+		return $(elem).text();
+	    };
+
+	    //	    console.log("dumping subjects: ");
+	    //	    console.dir($('[rdf\\:resource$="/LCSH"] ~ rdf\\:value').map(collect).get());
+	    //	    console.dir($('pgterms\\:ebook dcterms\\:subject rdf\\:value').map(collect).get());
+	    //	    console.log("done dumping subjects");
+
+
+	    callback(null, {
+		    _id: $('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', ''),
+			title: $('dcterms\\:title').text(),
+			authors: $('pgterms\\:agent pgterms\\:name').map(collect).get(),
+			subjects: $('pgterms\\:ebook dcterms\\:subject rdf\\:value').map(collect).get()
+			});
+	});
+};
+
+			   
